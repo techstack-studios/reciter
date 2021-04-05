e101d52e332664fd50e369ccad9fa8dfec05d100
Rice <50019781+beijingrice@users.noreply.github.com>
2021-04-05 21:17:42 +0800

    Add files via upload

--- Reciter++/main.py
+++ Reciter++/main.py
@@ -1,57 +1,75 @@
+from tkinter import *
+import tkinter.messagebox
 import json
+import sys
 import os
-import tkinter
-from tkinter import Frame, Button, Label, Listbox, Menu, Entry, messagebox, END
 
-# 初始化
+"""Init"""
+tk = Tk()
 
-directory = os.path.dirname(__file__)  # 程序目录
-plan_selected = False  # 计划是否被选择
-default_language = 0  # 0-zh_cn, 1-en_us
-test_started = False  # 测试是否开始
-plans_file_exists = False  # 计划文件是否存在
-language_filename = os.path.join(directory, "assets", "locale.json")  # 语言文件所在目录
-plans_filename = "plans_file.json"  # 计划文件
+plan_selected = False
+language_filename_txt = "current_language.txt"
+try:
+    with open(language_filename_txt, "r") as obj:
+        default_language = int(obj.read())
+except FileNotFoundError:
+    default_language = 0
+tk_ui_language_var = IntVar()
+tk_ui_language_var.set(default_language)
+# 0-chn, 1-eng
+is_test_start = 0  # 0-not start   1-start
+plans_file_exists = False
+language_filename = "language_file.json"
+plans_filename = "plans_file.json"
 
-# 读入语言文件
 with open(language_filename, "r", encoding="utf-8") as obj:
     language_metadata = json.loads(obj.read())
 
-# 界面属性设置
-tk = tkinter.Tk()
-tk.title(language_metadata["TITLE"][default_language])  # 窗口标题
-tk.iconbitmap(os.path.join(directory, "assets", "AppIcon.ico"))  # 窗口图标
+tk.title(language_metadata["Title"][default_language])
 
-# 读入计划
-current_plan_name = ""  # 当前计划
-plans = []  # 计划列表
+current_plan_name = ""
+plans = []
 all_files = []
-all_files1 = os.listdir()  # 读取目录下所有文件
-for each in all_files1:  # 循环遍历
-    if ".json" in each and each != "language_file.json":  # 判断是否不是语言文件
-        all_files.append(each.replace(".json", ""))  # 将计划文件加入计划列表
+all_files1 = os.listdir()
+for each in all_files1:
+    if ".json" in each and each != "language_file.json":
+        all_files.append(each.replace(".json", ""))
 plans = all_files
 
-# 主目录
-master_menu = Menu(tk)  # 创建主窗口
-file_menu = Menu(master_menu)  # 窗口菜单
+""" --- """
+
+"""Master Menu"""
+
+
+def write_2_language_file():
+    with open(language_filename_txt, "w+") as obj:
+        obj.write(str(tk_ui_language_var.get()))
+    tkinter.messagebox.showwarning(language_metadata["Title"][default_language],
+                                   language_metadata["Restart 2 Apply"][default_language])
+
+
+master_menu = Menu(tk)
+file_menu = Menu(master_menu)
+file_menu.add_radiobutton(label="中文 Chinese", variable=tk_ui_language_var, value=0, command=write_2_language_file)
+file_menu.add_radiobutton(label="英语 English", variable=tk_ui_language_var, value=1, command=write_2_language_file)
 master_menu.add_cascade(
-    label=language_metadata["FILE_MENU"][default_language], menu=file_menu)  # 加入“帮助”菜单
-tk.config(menu=master_menu)  # 设置菜单
+    label=language_metadata["Language Menu"][default_language], menu=file_menu)
+tk.config(menu=master_menu)
+""" --- """
 
+"""Plans View"""
 
-# 计划
 
 def new_plan():
     global plans
-    plan_name = entry1.get()
-    if plan_name == '':  # 没有计划名
-        tkinter.messagebox.showwarning(language_metadata["TITLE"][default_language],
-                                       language_metadata["CantBeEmpty"][default_language])  # 警告用户计划名不能为空
-    else:  # 有计划名
-        plans.append(plan_name)  # 添加计划
-        refresh_listbox(LB1, plans)  # 刷新计划列表
-        entry1.delete(0, END)  # idk
+    plan_name = E1.get()
+    if plan_name == '':
+        tkinter.messagebox.showwarning(language_metadata["Title"][default_language],
+                                       language_metadata["CantBeEmpty"][default_language])
+    else:
+        plans.append(plan_name)
+        refresh_listbox(LB, plans)
+        E1.delete(0, END)
 
 
 def get_current_plan(event):
@@ -59,7 +77,7 @@ def get_current_plan(event):
     index_of_words = 0
     plan_selected = True
     try:
-        current_plan_name = LB1.get(LB1.curselection()[0])
+        current_plan_name = LB.get(LB.curselection()[0])
     except IndexError:
         pass
     try:
@@ -71,19 +89,42 @@ def get_current_plan(event):
     refresh_listbox(LB1, keys)
 
 
-frame1 = Frame(tk, width=200, height=200)  # 框架1
-frame1.grid(row=0, column=0)
-LB1 = Listbox(frame1)  # 多行文本框 ——计划列表
-LB1.grid(row=0, column=0)
-LB1.bind("<<ListboxSelect>>", func=get_current_plan)  # 绑定事件 ——当前事件更新
-entry1 = Entry(frame1)  # 文本框1
-entry1.grid(row=1, column=0)
-bottom2 = Button(
-    frame1, text=language_metadata["Create New Plan"][default_language], command=new_plan)  # 按钮2 ——创建计划
-bottom2.grid(row=2, column=0)
+def del_selected_plan():
+    global plans, word_li
+    try:
+        plans.remove(current_plan_name)
+    except ValueError:
+        pass
+    refresh_listbox(LB, plans)
+    try:
+        os.remove(current_plan_name + ".json")
+    except FileNotFoundError:
+        pass
+    word_li = {}
+    set_keys_2_word_li()
+    refresh_listbox(LB1, keys)
+
+
+F1 = Frame(tk, width=200, height=200)
+F1.grid(row=0, column=0)
+LB = Listbox(F1)
+LB.grid(row=0, column=0)
+LB.bind("<<ListboxSelect>>", func=get_current_plan)
+E1 = Entry(F1)
+E1.grid(row=1, column=0)
+B2 = Button(
+    F1, text=language_metadata["Create New Plan"][default_language], command=new_plan)
+B2.grid(row=2, column=0)
+B5 = Button(
+    F1, text=language_metadata["Del Selected Plan"][default_language], command=del_selected_plan
+)
+B5.grid(row=3, column=0)
+
 
 for each in plans:
-    LB1.insert(END, each)  # 将计划加入计划列表
+    LB.insert(END, each)
+
+""" --- """
 
 # For debug
 word_li = {}
@@ -99,12 +140,10 @@ def set_keys_2_word_li():
         keys.append(each[0])
 
 
-# 测试结果反馈
 def show_result():
     global index_of_words, crosses, ticks
     E.delete(0, END)
-    result = tkinter.Tk()
-    result.iconbitmap(os.path.join(directory, "assets", "AppIcon.ico"))
+    result = Tk()
     result.title(language_metadata["Result"][default_language])
     tick_l = Label(
         result, text=language_metadata["Ticks"][default_language] + str(ticks))
@@ -123,29 +162,30 @@ def show_result():
 
 
 def check_if_finished():
-    global test_started
+    global is_test_start
     if index_of_words == len(keys) - 1:
-        test_started = 0
+        is_test_start = 0
         return True
-    else:
-        return False
 
 
 def update_word():
-    global test_started
+    global is_test_start
 
     try:
         L2.configure(text=keys[index_of_words])
-        test_started = 1
+        is_test_start = 1
     except IndexError:
-        tkinter.messagebox.showwarning(language_metadata["TITLE"][default_language],
-                                       language_metadata["Plz ChooseP"][default_language])
-        pass
+        if keys != []:
+            tkinter.messagebox.showwarning(language_metadata["Title"][default_language],
+                                           language_metadata["Plz ChooseP"][default_language])
+        else:
+            tkinter.messagebox.showwarning(language_metadata["Title"][default_language],
+                                           language_metadata["Plz AddWord"][default_language])
 
 
 def confirm():
-    global index_of_words, ticks, crosses, test_started
-    if test_started == 0:
+    global index_of_words, ticks, crosses, is_test_start
+    if is_test_start == 0:
         tkinter.messagebox.showwarning(language_metadata["Title"][default_language],
                                        language_metadata["PlzStartTest"][default_language])
     else:
@@ -163,7 +203,7 @@ def confirm():
 
 def skip():
     global index_of_words, crosses
-    if test_started == 0:
+    if is_test_start == 0:
         tkinter.messagebox.showwarning(language_metadata["Title"][default_language],
                                        language_metadata["PlzStartTest"][default_language])
     else:
@@ -180,30 +220,61 @@ def skip():
 
 
 """Words List View"""
-frame3 = Frame(tk, width=200, height=200)
-frame3.grid(row=0, column=1)
-left_box2 = Listbox(frame3)
-left_box2.grid(row=0, column=0)
-E2 = Entry(frame3)
+F3 = Frame(tk, width=200, height=200)
+F3.grid(row=0, column=1)
+LB1 = Listbox(F3)
+LB1.grid(row=0, column=0)
+E2 = Entry(F3)
 E2.grid(row=1, column=0)
-L = Label(frame3, text=language_metadata["Chinese"][default_language])
+L = Label(F3, text=language_metadata["Chinese"][default_language])
 L.grid(row=2, column=0)
-E3 = Entry(frame3)
+E3 = Entry(F3)
 E3.grid(row=3, column=0)
-L1 = Label(frame3, text=language_metadata["English"][default_language])
+L1 = Label(F3, text=language_metadata["English"][default_language])
 L1.grid(row=4, column=0)
+
+current_select_word = ""
+
+
+def get_current_selected_word(event):  # 绑定被选中事件
+    global current_select_word
+    try:
+        current_select_word = LB1.get(LB1.curselection()[0])  # 被选中后自动更新当前被选中词
+    except IndexError:  # 如无词 防报错
+        pass
+
+
+def del_selected_word():  # 删除选中词汇
+    global word_li, keys, index_of_words, current_plan_name
+    """
+    1. 从word_li字典中移除
+    2. 更新keys
+    3. index_of_words -= 1
+    4. 使用current_plan_name存盘
+    5. 更新UI
+    """
+    # current_selected_word为英文 是键
+    try:
+        del word_li[current_select_word]
+    except KeyError:
+        pass
+    set_keys_2_word_li()
+    index_of_words -= 1
+    with open(current_plan_name + ".json", "w+") as obj:
+        obj.write(json.dumps(word_li))
+    refresh_listbox(LB1, keys)
 
 
 def add_word():
     if not plan_selected:
         tkinter.messagebox.showwarning(
-            language_metadata["TITLE"][default_language], language_metadata["Plz CP"][default_language])
+            language_metadata["Title"][default_language], language_metadata["Plz CP"][default_language])
     else:
         global word_li
         cur_word_chn = E2.get()
         cur_word_eng = E3.get()
         if cur_word_chn == '' or cur_word_eng == '':
-            tkinter.messagebox.showwarning(language_metadata["TITLE"][default_language],
+            tkinter.messagebox.showwarning(language_metadata["Title"][default_language],
                                            language_metadata["CantBeEmpty"][default_language])
         else:
             word_li[cur_word_eng] = cur_word_chn
@@ -219,8 +290,12 @@ def add_word():
 
 
 B3 = Button(
-    frame3, text=language_metadata["Add New Word"][default_language], command=add_word)
+    F3, text=language_metadata["Add New Word"][default_language], command=add_word)
 B3.grid(row=5, column=0)
+B4 = Button(
+    F3, text=language_metadata["Del Selected Word"][default_language], command=del_selected_word)
+B4.grid(row=6, column=0)
+LB1.bind("<<ListboxSelect>>", func=get_current_selected_word)
 
 """ --- """
 
@@ -228,14 +303,16 @@ B3.grid(row=5, column=0)
 
 F2 = Frame(tk, width=200, height=200)
 F2.grid(row=0, column=2)
-B4 = Button(F2, text=language_metadata["Start Exam"][default_language], command=update_word)
+B4 = Button(F2, text=language_metadata["Start Exam"]
+[default_language], command=update_word)
 B4.grid(row=0, column=0)
 L2 = Label(F2, text="")
 L2.grid(row=1, column=0)
 L2.configure(text="")
 E = Entry(F2)
 E.grid(row=2, column=0)
-B = Button(F2, text=language_metadata["CONFIRM"][default_language], command=confirm)
+B = Button(F2, text=language_metadata["Confirm"]
+[default_language], command=confirm)
 B.grid(row=3, column=0)
 B1 = Button(F2, text=language_metadata["Skip"][default_language], command=skip)
 B1.grid(row=4, column=0)
